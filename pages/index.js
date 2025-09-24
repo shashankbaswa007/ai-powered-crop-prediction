d1e62eeb084c75ac6a93803b9512ef35a49c4343
BASWA SHASHANK SAI SRI <baswashashank123@gmail.com>
2025-09-24 11:08:47 +0530

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import { useAppContext } from '../context/AppContext';
 import Navbar from '../components/Navbar';            // default export
 import Dashboard from '../components/Dashboard';     // default export
 import PredictionForm from '../components/PredictionForm'; // default export
+// Home (index.js) should have)
+import Card from '../components/Card';
 import Chatbot from '../components/Chatbot';         // default export
 
 const Home = () => {
